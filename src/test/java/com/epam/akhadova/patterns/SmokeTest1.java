597130b250ceb6064b1f54b51a2403251ffe930b
Viktoria <gluchka@ukr.net>
2018-04-16 23:08:39 +0300

    test run on jenkins2

--- src/test/java/com/epam/akhadova/patterns/SmokeTest1.java
+++ src/test/java/com/epam/akhadova/patterns/SmokeTest1.java
@@ -8,7 +8,7 @@ import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 @Log4j
-public class SmokeTest extends BaseTest {
+public class SmokeTest1 extends BaseTest {
 
     @BeforeTest
     public void setupComponents() {
@@ -19,20 +19,10 @@ public class SmokeTest extends BaseTest {
 
     @Test
     public void test1() {
-        String searchFor = "Samsung";
+        String searchFor = "Motorola";
         getDriver().get(getMainUrl());
         elmirHeaderForm.search(searchFor);
-        Assert.assertTrue(getDriver().getCurrentUrl().contains("Saaaamsung"));
+        Assert.assertTrue(getDriver().getCurrentUrl().contains(searchFor));
     }
-
-
-//    @Test
-//    public void test2() {
-//        String searchFor = "Motorola";
-//        getDriver().get(getMainUrl());
-//        elmirHeaderForm.search(searchFor);
-//        Assert.assertTrue(searchPage.getListOfProductNames().stream().anyMatch(name -> name.contains(searchFor)));
-//    }
-
 
 }
